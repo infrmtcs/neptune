6198a9889ec747a8e3f975ab36811bba4eefdd20
Duong Thanh Dat <DuongDat.Informatics@rocketmail.com>
2014-06-18 20:55:32 +0700

    Fix Foreign key in 2 models.py files

--- confession/models.py
+++ confession/models.py
@@ -4,8 +4,8 @@ from userlogin.models import User
 import datetime
 
 class Post(models.Model):
-    author = models.ForeignKey(User)
-    receiver = models.ForeignKey(User)
+    author = models.ForeignKey(User, related_name = 'author')
+    receiver = models.ForeignKey(User, related_name = 'receiver')
     content = models.TextField()
     postedtime = models.DateTimeField()
     deadline = models.DateTimeField()
--- userlogin/models.py
+++ userlogin/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-class User():
+class User(models.Model):
     fb_id = models.CharField(max_length = 200)
+    postcount = models.IntegerField(default = 0)
     
     def __str__(self):
         return self.fb_id
