8278333d59aac89c28927b00e734560462cf8634
Duong Thanh Dat <DuongDat.Informatics@rocketmail.com>
2014-06-18 17:17:50 +0700

    Create table User and Post in models.py files

--- confession/models.py
+++ confession/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.forms import DateField
+from userlogin.models import User
+import datetime
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    receiver = models.ForeignKey(User)
+    content = models.TextField()
+    postedtime = models.DateTimeField()
+    deadline = models.DateTimeField()
+    
+    def __str__(self):
+        return self.content
--- userlogin/models.py
+++ userlogin/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-
+class User():
+    fb_id = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.fb_id
+		
