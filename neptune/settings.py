b92c140d1a703ac2667153306c26f67090829ea4
Duong Thanh Dat <DuongDat.Informatics@rocketmail.com>
2014-06-18 16:50:48 +0700

    Clean

--- neptune/settings.py
+++ neptune/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'confession',
+    'userlogin',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,12 @@ WSGI_APPLICATION = 'neptune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Neptune',
+        'USER': 'neptune',
+        'PASSWORD': '1nam2tuan',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
