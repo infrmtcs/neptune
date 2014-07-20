0b5b6eaaaa50cd311ad66bec6171087d32e01b07
Duong Thanh Dat <DuongDat.Informatics@rocketmail.com>
2014-07-20 22:45:30 +0700

    Add js functions to get data from FB

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,30 @@
+function postToURL(url, values) {
+	values = values || {};
+	var form = document.createElement("form", {action: url, method: "POST", style: "display: none"});
+	for (var property in values) {
+		if (values.hasOwnProperty(property)) {
+			var value = values[property];
+			if (value instanceof Array) {
+				for (var i = 0, l = value.length; i < l; i++) {
+					form.appendChild(document.createElement("input", {type: "hidden", name: property, value: value[i]}));
+				}
+			} else {
+				form.appendChild(document.createElement("input", {type: "hidden", name: property, value: value}));
+			}
+		}
+	}
+	document.body.appendChild(form);
+	form.submit();
+	document.body.removeChild(form);
+}
+
 function statusChangeCallback(response) {
 	if (response.status == "connected") {
-		console.log(response.authResponse.userID);
+		FB.api("/me",
+			function (response) {
+				postToURL("/confession/index", response);
+			}
+		);
 	} else {
 	}
 }
